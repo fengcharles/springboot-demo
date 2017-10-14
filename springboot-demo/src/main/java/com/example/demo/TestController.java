b95dd500ded27a0f000013d397689c1854cf3be5
fyp <charles@mogoroom.com>
2017-10-14 11:18:32 +0800

    新增springboot-demo

---
+++ springboot-demo/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping(value = "/hello")
+public class TestController {
+
+    @RequestMapping
+    public String hello() {
+        return "Hello Spring-Boot";
+    }
+
+    @RequestMapping("/info")
+    public Map<String, String> getInfo(@RequestParam String name) {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", name);
+        return map;
+    }
+
+
+}
